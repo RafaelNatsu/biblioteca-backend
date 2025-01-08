425aef2c477e8f56400f9f164eb863bc12463730
RafaelNatsu <rafaelnatsu99@gmail.com>
2025-01-08 20:37:15 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -8,6 +9,15 @@ async function bootstrap() {
     app.enableVersioning({
         type: VersioningType.URI
     });
+
+    const config = new DocumentBuilder()
+    .setTitle('Biblioteca backend')
+    .setDescription('Sistema de gerenciamento de biblioteca')
+    .setVersion('v1')
+    .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
+
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
