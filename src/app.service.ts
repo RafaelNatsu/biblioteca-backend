e9fd1990737c1bb0276361666a1aad23ae4b6caf
RafaelNatsu <rafaelnatsu99@gmail.com>
2025-01-08 20:27:47 -0300

    feat: Add API versioning

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,6 @@ import { Injectable, Logger } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        Logger.log('hello world!');
-        return 'Hello World!s2';
+        return 'Hello World!';
     }
 }
